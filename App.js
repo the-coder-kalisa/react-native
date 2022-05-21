e985d351e19eb2ab55d858c281c6a0e7c0842521
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-05-21 20:58:15 +0200

    'hello'

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import React, {useState, useEffect} from 'react'
+import { StatusBar, Text, View } from 'react-native';
+import tw from 'twrnc'
 export default function App() {
+  const [currUser, setCurrUser] = useState(null);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`mt-[${StatusBar.currentHeight}px] h-full w-full flex items-center justify-center`}>
+      <Text style={tw`font-bold text-2xl`}>hello world!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
